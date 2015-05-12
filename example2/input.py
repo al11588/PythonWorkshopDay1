ffd7ff26a230d5d212202c7058321d833b96ec1b
al11588 <al11588@gmail.com>
2015-05-11 21:15:48 -0400

    Update input.py

--- example2/input.py
+++ example2/input.py
@@ -1,3 +1,3 @@
-raw_input("This is Python 2") # This is this is Python 2 that returns a string
+raw_input("This is Python 2") # This is Python 2 that returns a string
 
 input(" This is Python 3") #This is Python 3 that tries to run inputs as an expression. 
